1509fbe36bb173ebbdf66f1d116a5a28818fe506
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-03-17 09:27:00 +0000

    Initial commit

---
+++ src/test/java/ma/n7/hopital/HopitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.n7.hopital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
